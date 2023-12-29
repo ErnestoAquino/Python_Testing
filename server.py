7f3000317636e8c861ebe2f3482358cc013b985c
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-12-29 13:26:32 +0100

    Fix issue #2: Prevent clubs from using more points than available

--- server.py
+++ server.py
@@ -3,24 +3,41 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 
 from constants import EMAIL_NOT_FOUND_ERROR
 from constants import EMAIL_EMPTY_ERROR
+from constants import INSUFFICIENT_PLACES_MESSAGE
+from constants import INSUFFICIENT_POINTS_MESSAGE
+from constants import BOOKING_COMPLETE_MESSAGE
+from constants import INVALID_PLACES_MESSAGE
+from constants import NON_POSITIVE_PLACES_MESSAGE
+from constants import INVALID_POINTS_MESSAGE
 
 
 def loadClubs():
     with open('clubs.json') as c:
-        listOfClubs = json.load(c)['clubs']
-        return listOfClubs
+        list_of_clubs = json.load(c)['clubs']
+        return list_of_clubs
 
 
 def loadCompetitions():
     with open('competitions.json') as comps:
-        listOfCompetitions = json.load(comps)['competitions']
-        return listOfCompetitions
+        list_of_competitions = json.load(comps)['competitions']
+        return list_of_competitions
+
+
+def save_clubs(club_list):
+    with open('clubs.json', 'w') as c:
+        json.dump({"clubs": club_list}, c, indent=4)
+
+
+def save_competitions(competition_list):
+    with open('competitions.json', 'w') as comps:
+        json.dump({"competitions": competition_list}, comps, indent=4)
 
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
-competitions = loadCompetitions()
+# competitions = loadCompetitions()
+# clubs = loadClubs()
 
 
 @app.route('/')
@@ -32,6 +49,7 @@ def index():
 def showSummary():
     email = request.form.get('email')
     clubs = loadClubs()
+    competitions = loadCompetitions()
 
     if not email:
         flash(EMAIL_EMPTY_ERROR)
@@ -48,6 +66,7 @@ def showSummary():
 @app.route('/book/<competition>/<club>')
 def book(competition, club):
     clubs = loadClubs()
+    competitions = loadCompetitions()
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
@@ -60,12 +79,50 @@ def book(competition, club):
 @app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     clubs = loadClubs()
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    competitions = loadCompetitions()
+    selected_competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+    selected_club = [c for c in clubs if c['name'] == request.form['club']][0]
+
+    # Check that the number of places requested is valid.
+    try:
+        places_required = int(request.form['places'])
+        if places_required <= 0:
+            flash(NON_POSITIVE_PLACES_MESSAGE)
+            return redirect(url_for('book', competition=selected_competition['name'], club=selected_club['name']))
+        points_to_use = places_required
+    except ValueError:
+        flash(INVALID_PLACES_MESSAGE)
+        return redirect(url_for('book', competition=selected_competition['name'], club=selected_club['name']))
+
+    # Check if the club has enough points.
+    try:
+        available_points = int(selected_club['points'])
+        if available_points < places_required:
+            flash(INSUFFICIENT_POINTS_MESSAGE)
+            return redirect(url_for('book', competition=selected_competition['name'], club=selected_club['name']))
+    except ValueError:
+        flash(INVALID_POINTS_MESSAGE)
+        return redirect(url_for('book', competition=selected_competition['name'], club=selected_club['name']))
+
+    # Check if there are enough places available in the competition.
+    try:
+        number_of_places = int(selected_competition['numberOfPlaces'])
+    except ValueError:
+        flash('Invalid number of places')
+        return redirect(url_for('book', competition=selected_competition['name'], club=selected_club['name']))
+    if number_of_places < points_to_use:
+        flash(INSUFFICIENT_PLACES_MESSAGE)
+        return redirect(url_for('book', competition=selected_competition['name'], club=selected_club['name']))
+
+    # If the checks are correct, we update the points and places.
+    selected_club['points'] = available_points - points_to_use
+    selected_competition['numberOfPlaces'] = number_of_places - places_required
+
+    save_clubs(clubs)
+    save_competitions(competitions)
+
+    flash(BOOKING_COMPLETE_MESSAGE)
+    return render_template('welcome.html', club=selected_club, competitions=competitions)
 
 
 # TODO: Add route for points display
