49d66e96d518148346866c6915e9d9b598f475fb
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-12-29 13:25:41 +0100

    feat: add constants for purchase places messages and validations

--- constants.py
+++ constants.py
@@ -1,2 +1,10 @@
 EMAIL_NOT_FOUND_ERROR = "Sorry, that email was not found. Please try again."
 EMAIL_EMPTY_ERROR = "Please introduce an email."
+
+# Messages for purchase places
+INSUFFICIENT_POINTS_MESSAGE = "You do not have enough points to reserve these places."
+INSUFFICIENT_PLACES_MESSAGE = "There are not enough places available for this competition."
+BOOKING_COMPLETE_MESSAGE = "Great-booking complete!"
+INVALID_PLACES_MESSAGE = "Invalid number of places requested."
+INVALID_POINTS_MESSAGE = "Invalid points value for the club"
+NON_POSITIVE_PLACES_MESSAGE = "Number of places requested must be greater than zero."
