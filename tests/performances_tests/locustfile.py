0474df0f270b1b250202824f9391935a3c10ca2e
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-01-12 14:52:07 +0100

    perf: implement comprehensive Locust performance tests for user actions

--- tests/performances_tests/locustfile.py
+++ tests/performances_tests/locustfile.py
@@ -1,12 +1,40 @@
 from locust import HttpUser
 from locust import task
 from locust import between
+from locust import TaskSet
 
 
-class ProjectPerformanceTest(HttpUser):
-    host = "http://127.0.0.1:5000"
-    wait_time = between(1, 2)
+class ClubUserBehavior(TaskSet):
+    @task
+    def view_table_club_points(self):
+        self.client.get("/club-points")
 
     @task
-    def load_club_main_page(self):
-        self.client.get("/club-points")
+    def load_main_page(self):
+        self.client.get("/")
+
+    @task
+    def show_summary(self):
+        self.client.post("/showSummary", {"email": "contact@highflyers.com"})
+
+    @task
+    def book_competition(self):
+        self.client.get("/book/Test Championship/High Flyers")
+
+    @task
+    def purchase_places(self):
+        self.client.post("/purchasePlaces", {
+            "competition": "Test Championship",
+            "club": "High Flyers",
+            "places": "1"
+        })
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
+
+
+class WebsiteUser(HttpUser):
+    host = "http://127.0.0.1:5000"
+    tasks = [ClubUserBehavior]
+    wait_time = between(1, 5)
